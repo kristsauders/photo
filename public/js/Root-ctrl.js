064bdc6eba7bb63ff62307fd9cf24857a3ce0a25
Krists Auders <kristsauders@gmail.com>
2013-05-15 16:16:12 -0600

    Update Root-ctrl.js

--- public/js/Root-ctrl.js
+++ public/js/Root-ctrl.js
@@ -88,6 +88,9 @@ function RootCtrl($scope, $location, $timeout) {
                 }
             }
     });
+    
+    // Prevent links opening in safari when in full screen web app mode on iPhone
+    (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")
 
 }
 RootCtrl.$inject = ['$scope', '$location', '$timeout'];
