65831a2187a374319c7330bae6445fb63240e4dc
Krists Auders <kristsauders@gmail.com>
2013-05-20 18:13:30 -0600

    Update deploy_to_appfog.sh

--- deploy_to_appfog.sh
+++ deploy_to_appfog.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 export CLOUDFOUNDRY_USERNAME=kristsauders@gmail.com
+export CLOUDFOUNDRY_PASSWORD="Is this good enough?"
 if [ -z "$CLOUDFOUNDRY_PASSWORD" ]; then
     echo "CLOUDFOUNDRY_PASSWORD must be set to $CLOUDFOUNDRY_USERNAME's cloudfoundry password "
     echo "==== Your current environment====="
     env
     exit 1
 fi
-gem install vmc
-vmc target https://api.cloudfoundry.com
-vmc login $CLOUDFOUNDRY_USERNAME --password $CLOUDFOUNDRY_PASSWORD
-vmc push cf-docs-contrib
+gem install af
+af target https://api.appfog.com
+af login $CLOUDFOUNDRY_USERNAME --password $CLOUDFOUNDRY_PASSWORD
+af update photo
