4f4f36ff02cc9145550d51ec0a8df300d28c8ef2
Krists Auders <kristsauders@gmail.com>
2013-05-20 18:29:44 -0600

    Update deploy_to_appfog.sh

--- deploy_to_appfog.sh
+++ deploy_to_appfog.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export CLOUDFOUNDRY_USERNAME=kristsauders@gmail.com
-export CLOUDFOUNDRY_PASSWORD="Is this good enough?"
+export CLOUDFOUNDRY_PASSWORD=HowIsThis?
 if [ -z "$CLOUDFOUNDRY_PASSWORD" ]; then
     echo "CLOUDFOUNDRY_PASSWORD must be set to $CLOUDFOUNDRY_USERNAME's cloudfoundry password "
     echo "==== Your current environment====="
@@ -9,5 +9,5 @@ if [ -z "$CLOUDFOUNDRY_PASSWORD" ]; then
 fi
 gem install af
 af target https://api.appfog.com
-af login --email $CLOUDFOUNDRY_USERNAME --passwd "$CLOUDFOUNDRY_PASSWORD"
+af login --email $CLOUDFOUNDRY_USERNAME --passwd $CLOUDFOUNDRY_PASSWORD
 af update photo
